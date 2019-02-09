0202d43fc72acfe2d9f1e1eb00c70309a6f45970
SoftTacos <onlykindanerdy@gmail.com>
2019-02-09 13:35:49 -0700

    Suicide burn script now lands craft safely. It does not yet control gyros or non-aligned thrusters, but it does land vertical craft.

--- SpaceEngineersScripts/SuicideBurn.cs
+++ SpaceEngineersScripts/SuicideBurn.cs
@@ -1,12 +1,11 @@
 ﻿/* Using this script as a learning base for thrust management and learning to "fly"
  * Script will calculate the height at which to ensurethrusters must be used to slow down and not hit the ground
  * currently uses lazy shortcuts and does not actually touch down, that's TODO. Just wanted something that works and has the fundamental math done right
- * TODO: manage gyros to orient craft straight down
+ * TODO: manage gyros to orient craft
  * TODO: manage thrusters in all directions to maneuver
- * TODO: actually land.
+ * TODO: actually land. DONE.
  * 
- * /
-
+ */
 
 using Sandbox.Game.EntityComponents;
 using Sandbox.ModAPI.Ingame;
@@ -30,145 +29,165 @@ namespace IngameScript
     partial class SuicideBurn : MyGridProgram
     {
         //COPY FROM HERE
-        //float targetAlt = 100.0f;
-        float functionalMass = 0.0f;
+        
+        float physicalMass = 0.0f;
         Vector3D currentNaturalGravity;
-
-        public Program()
+        int stage = -1;//STG0: safe; STG1: freefall; STG2: Initial slowdown; STG3: slow controlled descent
+        double currentAltitude = 0.0;
+        IMyRemoteControl controller;
+        List<IMyThrust> thrusters;
+        int numThrusters = 0;
+        float maxTotalEffectiveThrust = 0.0f;
+        float safeVelocity = 7.0f;
+        //float minSafeAltitude = 50.0f;
+        IMyRadioAntenna antenna;
+        //Vector3D previousVelocity;
+        float freefallThreshold;
+        float craftHeight;//leaving as a const until I get a calculation working
+        float touchdownHeight;
+        MyShipVelocities lastVelocities;//linear and angular velocities
+        double deltaT = 0.0;
+        //IMyGridProgramRuntimeInfo runtimeInfo = null;
+        public Program()//constructor is called when you "compile" script ingame
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set RuntimeInfo.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
-            //Runtime.UpdateFrequency = UpdateFrequency.Update1;//update once every game tick. options are non,Update1,Update10,Update100, once
-        }
+            deltaT = Runtime.TimeSinceLastRun.TotalSeconds;//runtimeInfo.TimeSinceLastRun.TotalSeconds;
 
-        public void Save()
-        {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
-        }
-
-        public void Main(string argument, UpdateType updateSource)
-        {
-            Runtime.UpdateFrequency = UpdateFrequency.Update1;//once run, this script needs to control the craft
-            //grid is assumed perfectly normal to current gravity vector so don't need rotation or lateral motion. One step at a time
-            //thrust up until X altitude
-            //shut off thrust and IDs
-            //turn on IDs when at minimum safe suicide burn height
-            //if anything goes wrong, parachutes??
-
-            //get remote control 
             List<IMyRemoteControl> controllers = new List<IMyRemoteControl>();
             GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(controllers);
-            //lazily assuming one RC for now
-            IMyRemoteControl controller = controllers[0];
-            
-            //testing to see all the data I can collect with a remote control block
-            //Echo($"{controller.GetShipVelocities()}\n");//returns linear and angular velocity of the ship
-            //Echo($"{controller.GetNaturalGravity()}\n");//Gets the vector pointing in the direction of planetary gravity, not normalized
-            //Echo($"{controller.GetPosition()}\n");//this gets my world position. Handy
-            double currentAlt = 0.0;//current altitude relative to the ground
-            bool valid = controller.TryGetPlanetElevation(MyPlanetElevation.Surface, out currentAlt);
-            //Echo($"{elevation}\n");//this gets my world position. Handy
-            //Echo($"{controller.CalculateShipMass().PhysicalMass}");//can get base mass(no cargo), total mass, and physical mass(ship mass+cargo adjusted by inv mult)
+            controller = controllers[0];//lazilly assuming 1 RC for now
 
-            //get thruster(s), all pointing down atm
-            List<IMyThrust> thrusters = new List<IMyThrust>();
+            List<IMyRadioAntenna> antennas = new List<IMyRadioAntenna>();
+            GridTerminalSystem.GetBlocksOfType<IMyRadioAntenna>(antennas);
+            antenna = antennas[0];
+
+            thrusters = new List<IMyThrust>();
             GridTerminalSystem.GetBlocksOfType<IMyThrust>(thrusters);
-            float totalThrust = 0.0f;
-            int numThrusters=0;
-            foreach (var thruster in thrusters)
-            {
-                numThrusters++;
-                totalThrust += thruster.MaxEffectiveThrust;
-                //thruster.ThrustOverride = thruster.MaxThrust;
-            }
-            //calculate thrust needed to hover?
-            functionalMass = controller.CalculateShipMass().PhysicalMass;
-            currentNaturalGravity = controller.GetNaturalGravity();
-            double gravityMagnitude = currentNaturalGravity.Length();//what a weird thing to call magnitude
+            maxTotalEffectiveThrust = 0.0f;
+            numThrusters = thrusters.Count;
+            freefallThreshold = 10.0f;
 
-            double thrustNeededToHover = functionalMass / gravityMagnitude;
-            //I'm gonna mess up converting floats and doubles and someone's gonna die
-           
-            //this method won't work, but just want to do it for practice
-            /*
-            //thrust function: N = (x-(targetAlt-CurrentAlt))^3 + thrustNeededToHover//bound that by max thrust and min thrust
-            double currentTargetThrust = targetAlt-currentAlt + thrustNeededToHover;
-            if (currentTargetThrust < 0)
-                currentTargetThrust = 0.0;
-            Echo($"current total thrust output: {currentTargetThrust}");
-            //divide the total needed thrust by total functional thrusters, set each thruster to that amount of thrust
-            foreach (var thruster in thrusters)
-            {
-                thruster.ThrustOverride = (float)currentTargetThrust / numThrusters;
-            }
-            */
-
-            //make craft lift off(from a hover) and then suicide burn?
-
-            //what distance to stop == alt+Buffer => turn on thrust
-            /*
-            X = time to hit v=0
-            then integrate from t=0 -> X
-            that's stopping distance
-            Calc1 coming back to haunt me
-            */
-            //manually solving for time being just to get something working
-            double a = totalThrust / functionalMass - currentNaturalGravity.Length();
-            double b = controller.GetShipVelocities().LinearVelocity.Length();
-            double c = 0;// currentAlt;
-            //Echo($"{a}:{b}:{c}");
-            double thrustTime = b/(2*a);//y=ax^2+bx -> take derivative -> dy = 2ax+b | 0 = 2*a*x+b -> -b/2a=x//dropping the - on b because calculated B is absolute value and they would cancel irl
-
-            //i need to calc time to stop the ship at current speed, compare that to the alt, then integrate
-
-            Echo($"ThrustTime: {thrustTime}");
-            Func<double, double> altitude = delegate (double x)
-            {
-                return a * x * x + b * x + c;
-            };
-            int slices = 10;
-            double brakingHeight = integrate(0, thrustTime, altitude, slices);
-            Echo($"ALT: {currentAlt}");
-            Echo($"BRAKE: {brakingHeight}");
+            craftHeight = 5.0f;
+            touchdownHeight = craftHeight + 1;//adding wiggle room because craft tends to bounce and code the inputs don't always capture that
             
-            double safetyBuffer = b/10 ;
-            Echo($"safety buffer: {safetyBuffer}");
+            stage = 0;
+            Storage = stage + ";"; ;
 
-            //there are 60 "ticks" of ingame simulation per second
+            Echo($"Code Initialized");
 
-            if (currentAlt <= brakingHeight + safetyBuffer)
-            {
-                controller.DampenersOverride = true;
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;//once run, this script needs to control the craft actively
+
+        }
+
+        public void Save()//idk when this is called but storage needs to be updated just in case
+        {
+            Storage = stage + ";";
+            Echo($"Saved");
+        }
+
+        public void Main(string argument, UpdateType updateSource)//main is called once per pysics update, ideally 60Hz
+        {
+            if (argument.ToLower() == "reset")
+                stage = 0;
+            bool valid = controller.TryGetPlanetElevation(MyPlanetElevation.Surface, out currentAltitude);
+
+            var parts = Storage.Split(';');
+            stage = int.Parse(parts[0]);
+            
+            maxTotalEffectiveThrust = 0.0f;//have to recalculate every time because it increases with alt
+            foreach (var thruster in thrusters){
+                maxTotalEffectiveThrust += thruster.MaxEffectiveThrust;
+            }
+            physicalMass = controller.CalculateShipMass().PhysicalMass;
+            currentNaturalGravity = controller.GetNaturalGravity();//
+            double gravityMagnitude = currentNaturalGravity.Length();//what a weird thing to call magnitude of a vector
+            
+            Echo($"STAGE: {stage}\nSPEED: {controller.GetShipVelocities().LinearVelocity.Length()}\nALT: {currentAltitude}");
+            antenna.HudText = stage.ToString();
+            
+            if (stage == 0){
+                if (Freefall()){//if we are in freefall. Freefall is defined as accelerating at a rate ~= gravity, this implies thrusters are not acive. Also should check if it's heading towards or away from the gravity well. Linear alg time yay
+                    foreach(var thruster in thrusters){
+                        thruster.ThrustOverride = 0;
+                    }
+                    stage++;
+                    Storage = stage + ";";
+                }
+            }
+
+            else if(stage == 1 ){
+                if (!Freefall())
+                    stage = 0;
+                double a = maxTotalEffectiveThrust / physicalMass - currentNaturalGravity.Length();//acceleration with full retro burn
+                double b = -controller.GetShipVelocities().LinearVelocity.Length();//velocity
+                double c = currentAltitude;
+                double thrustStopTime = -b / (2 * a);//y=ax^2+bx ->  dy = 2ax+b | 0 = 2*a*x+b -> -b/2a=x
+
+                Func<double, double> altitudeFunc = delegate (double x) {//x=time, y=altitude assuming full retro thrust
+                    return a * x * x + b * x + c;
+                };
+
+                double minHeight = altitudeFunc(thrustStopTime);//the lowest point of the trajectory IF thrust is engaged instantly
+                double calculatedStoppingDistance = currentAltitude - minHeight;
+                double safetyBuffer = Math.Abs(b)*1.25+craftHeight;//a coefficient in front of the safety buffer feels wrong
+                double safetyStoppingDistance = calculatedStoppingDistance + safetyBuffer;
+               
+                if (!controller.DampenersOverride && currentAltitude <= safetyStoppingDistance){
+                    stage++;
+                    Storage = stage + ";";
+                    controller.DampenersOverride = true;
+                }
+            }
+
+            else if(stage == 2){//descent burn has been initiated, goto stg 3 when a safe speed has been reached
+                antenna.HudText = stage.ToString();
+                
+                if (controller.GetShipVelocities().LinearVelocity.Length() < safeVelocity) {
+                    stage++;
+                    Storage = stage + ";";
+                }
+            }
+
+            else if(stage == 3){//target safe descent speed has been reached, maintain low speed until touchdown to planet
+                float totalThrustNeededToHover = physicalMass * (float)currentNaturalGravity.Length();
+                float idealThrustPerThruster = (float)totalThrustNeededToHover / (float)numThrusters;
+                float thrustRatio = thrusters[0].MaxThrust / thrusters[0].MaxEffectiveThrust;//actual output of thrust is similar to but different from the input set by code/user. 
+                float adjustedThrustPerThruster = idealThrustPerThruster * thrustRatio;
+
+                foreach (var thruster in thrusters){
+                    thruster.ThrustOverride = adjustedThrustPerThruster;
+                }
+
+                if (currentAltitude < touchdownHeight){
+                    stage++;
+                    Storage = stage + ";";
+
+                    foreach (var thruster in thrusters)
+                    {
+                        controller.DampenersOverride = false;
+                        thruster.ThrustOverride = 0.0f;
+                    }
+                }
                 
             }
-            Echo($"DAMPENERS: {controller.DampenersOverride}");
-        }
 
-        public double integrate(double a, double b, Func<double, double> function, int n)//integrating manually because it's been 4 years
-        {
-            double area = 0.0;
-            for(int i = 0; i < n; i++)
-            {
-                double start = ((b - a) / n) * i;
-                double end = ((b - a) / n) * (i+1);
-                area += (end - start) * (function(end) + function(start)) / 2;//( x2 - x1 ) * ( y2 - y1 )/2
+            else if(stage == 4){//touchdown, turn stuff off pls
+                antenna.HudText = stage.ToString();
+                Echo($"TOUCHDOWN");
+                stage = 0;
+                Storage = stage + ";";
+                Runtime.UpdateFrequency = UpdateFrequency.None;//stop running the script
             }
-            return area;
+            lastVelocities = controller.GetShipVelocities();
         }
 
+        public bool Freefall(){
+            double tolerance = 0.5;//TODO
+            Vector3D currentAcceleration = ( controller.GetShipVelocities().LinearVelocity - lastVelocities.LinearVelocity ) / Runtime.TimeSinceLastRun.TotalSeconds;
+            Echo($"DIFF:{controller.GetNaturalGravity() - currentAcceleration}");
+            if (Math.Abs(controller.GetNaturalGravity().X - currentAcceleration.X) < tolerance && Math.Abs(controller.GetNaturalGravity().Y - currentAcceleration.Y) < tolerance && Math.Abs(controller.GetNaturalGravity().Z - currentAcceleration.Z) < tolerance)
+                return true;
+            return false;
+        }
         //COPY FROM HERE
     }
 }
