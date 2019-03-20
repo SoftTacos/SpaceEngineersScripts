6d6e7f398fb749a7103d8747907c0a7ac1422856
SoftTacos <onlykindanerdy@gmail.com>
2019-03-20 15:42:44 -0600

    AutomatedMissileDefense uses rudimentary logic to target missiles with turrets. Quick and dirty atm, no target tracking so it will spam missiles at one thing.

--- SpaceEngineersScripts/AutomatedMisssileDefense.cs
+++ SpaceEngineersScripts/AutomatedMisssileDefense.cs
@@ -21,28 +21,65 @@ namespace IngameScript
     //MUCH less fancy than it sounds, this script manages the targeting data for specified turrets and triggers the launch code for LIDAR missiles. It does not do any of the missile guidance.
     partial class Program : MyGridProgram
     {
+        //keep this the same as your lidar designator tag for easy use
+        string designatorNameTag = "R_DESIGNATOR";
+        //what tag to look for on the programmable block that controls the missile.
+        string missileComputerTag = "TGASM";//name tag on current test missile script "Turret Guided Anti Ship Missile"
 
-        string designatorNameTag = "Designator";
-        List<IMyLargeTurretBase> designator;
+        List<IMyProgrammableBlock> missileComputers;
+        List<IMyLargeTurretBase> designators;
+        List<MyDetectedEntityInfo> targets;
 
-        string launchControlNameTag = "LCS";
-        List<IMyProgrammableBlock>;
+        //nice little visualizer to tell me the script is just idle
+        List<char> runningStrings = new List<char>() { '|', '/', '-', '\\' };
+        int runningIndex = 0;
         public Program()
         {
+            targets = new List<MyDetectedEntityInfo>();
+            designators = new List<IMyLargeTurretBase>();
+            missileComputers = new List<IMyProgrammableBlock>();
+
+            List<IMyLargeTurretBase> possibleDesignators = new List<IMyLargeTurretBase>();
+            GridTerminalSystem.GetBlocksOfType<IMyLargeTurretBase>(possibleDesignators);
+            foreach(IMyLargeTurretBase turret in possibleDesignators) {
+                if (turret.CustomName.Contains(designatorNameTag))
+                    designators.Add(turret);
+            }
 
             Runtime.UpdateFrequency = UpdateFrequency.Update1;
         }
 
-        public void Save()
-        {
-            
-        }
-
         public void Main(string argument, UpdateType updateSource)
         {
+            runningIndex++;
+            if (runningIndex >= runningStrings.Count)
+                runningIndex = 0;
+            Echo($"[{runningStrings[runningIndex]}]");
+
             //does turret have a target?
+            foreach (IMyLargeTurretBase designator in designators) {
+                if (designator.HasTarget) {
+                    targets.Add(designator.GetTargetedEntity());
+                }
+            }
             //if no, return
-            //if target find 
+            if (targets.Count == 0)//might need to add logic for determining if we want to shoot at those targets here
+                return;
+
+            //look for all programmable blocks of name NAME TAG and launch 1(no logic for now, just want POC)
+            List<IMyProgrammableBlock> possibleMissileComputers = new List<IMyProgrammableBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyProgrammableBlock>(possibleMissileComputers);
+            foreach (IMyProgrammableBlock computer in possibleMissileComputers){
+                if (computer.CustomName.Contains(missileComputerTag))
+                    missileComputers.Add(computer);
+            }
+            if (missileComputers.Count > 0) {
+                missileComputers[0].TryRun("");
+                Echo($"LAUNCHING: ");
+                Runtime.UpdateFrequency = UpdateFrequency.None;//turn it off for now for debugging
+            }//wipe all targets for now
+            targets.Clear();
+            missileComputers.Clear();
         }
     }
 }
--- SpaceEngineersScripts/PistonElevatorControl.cs
+++ SpaceEngineersScripts/PistonElevatorControl.cs
@@ -19,84 +19,64 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        //EXAMPLE SETUP: 
+        //This is the name of the group the pistons should belong to
         string PISTON_GROUP = "ElevatorPistons";
+        //character that separates the floor and it's corresponding height/ '\n' for newline
+        char keyDelimiter = ':';
+        //character that separates a floor+height pair. '\n' for newline
+        char pairDelimiter = ';';
+
+        //NO TOUCHY BELOW HERE
         List<IMyExtendedPistonBase> pistons;
         Dictionary<string, float> floors;//floor height will be indexed off of the pistons being at min length
-        float maxheight;
-        //specify direction? 
-        char keyDelimiter = ':';
-        char pairDelimiter = ';';
-        //handy ugly mapping of size of a single block to grid size since keen doesn't provide that
-        Dictionary<VRage.Game.MyCubeSize, float> GridSizeMapping = new Dictionary<MyCubeSize, float>() { { VRage.Game.MyCubeSize.Large, 2.5f }, { VRage.Game.MyCubeSize.Small, 0.5f } };
-
-        public Program()
-        {
-            //constructor
+        float maxHeight;
+        
+        public Program(){
             floors = new Dictionary<string, float>();
             floors = setupFloors(Me.CustomData);
-            foreach (string floor in floors.Keys){
-                Echo($"{floor}->{floors[floor]}");
-            }
             pistons = new List<IMyExtendedPistonBase>();
-            //GridTerminalSystem.GetBlocksOfType<IMyExtendedPistonBase>(pistons);
             GridTerminalSystem.GetBlockGroupWithName(PISTON_GROUP).GetBlocksOfType<IMyExtendedPistonBase>(pistons);
-            //maxheight = pistons.Count* GridSizeMapping[pistons[0].CubeGrid.GridSizeEnum]*2;
-            maxheight = 0.0f;
+            maxHeight = 0.0f;
             foreach(IMyExtendedPistonBase piston in pistons) {
-                maxheight += piston.HighestPosition;
+                maxHeight += piston.HighestPosition;
+            }
+            foreach(string floor in floors.Keys){
+                if (floors[floor] > maxHeight)
+                    Echo($"Height {floors[floor]} exceeds the maximum length of the elevator!");
             }
         }
 
-        public void Save()
-        {
-            
-        }
-
-        public void Main(string argument, UpdateType updateSource)
-        {
-            if (!floors.ContainsKey(argument))
-            {
+        public void Main(string argument, UpdateType updateSource){
+            if (!floors.ContainsKey(argument)){
                 Echo($"Invalid Argument! Recompile if you have changed the CustomData");
                 return;
             }
             float targetHeight = floors[argument];
-            //TODO: sanity check the target floor heights
-            //Echo($"{argument}:{Me.CustomData.Split(",")}");
-            //get current height
             float currentHeight = 0.0f;
             foreach (IMyExtendedPistonBase piston in pistons){
                 currentHeight += piston.CurrentPosition;
-                Echo($"{piston.Status}");
-                piston.MinLimit = targetHeight / pistons.Count;//doing it the quick n dirty way this time
+                piston.MinLimit = targetHeight / pistons.Count;//don't have to care about the piston's minlimit/maxlimit going out of bounds because SE handles that
                 piston.MaxLimit = targetHeight / pistons.Count;
-
             }
-            Echo($"CURRENT:{currentHeight}");
-            Echo($"TARGET:{targetHeight}");
-
+            
             //pistons can be below, above or at target
             float direction = targetHeight - currentHeight;
-            Echo($"DIR:{direction}");
             int multiplier = Math.Sign(direction*pistons[0].Velocity);
-            if (multiplier == 0)
-            {
-                Echo($"MULT=0 EXITING");
+            if (multiplier == 0){//shouldn't happen unless the player has set the velocity to 0, but juuuust in case
                 return;
             }
-            Echo($"{multiplier}");
             foreach (IMyExtendedPistonBase piston in pistons){
-                Echo($"{piston.Velocity}");
                 piston.Velocity = piston.Velocity*multiplier;
-                Echo($"{piston.Velocity}");
-
+                if(piston.Velocity > 0){
+                    piston.MinLimit = 0;
+                }
+                else{
+                    piston.MaxLimit = piston.HighestPosition;
+                }
             }
-
-            //TODO: If it's going up, set min to 0, if it's going down, set max to max value. This makes the script unintrusive to players who might not want to use it
         }
 
-        private Dictionary<string, float> setupFloors(string customData)
-        {
+        private Dictionary<string, float> setupFloors(string customData){
             Dictionary<string, float> newFLoors = new Dictionary<string, float>();
             string[] floorPears = customData.Split(pairDelimiter);
             foreach (string pair in floorPears){
